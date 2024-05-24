f72b822614e68d0ac0118a4df6e32f8c6eede692
Valentin Kim <valentink@yandex.ru>
2024-05-24 16:43:35 +0500

    errors fixed 4

--- celery_worker.py
+++ celery_worker.py
@@ -18,7 +18,7 @@ backend_url = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
 
 # app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
-app = Celery('tasks', broker=broker_url, backend=backend_url)
+app = Celery('tasks', broker="redis://redis:6379/0", backend="redis://redis:6379/0")
 
 app.conf.beat_schedule = {
     'process-orders-every-10-seconds': {
