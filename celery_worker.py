312f772d8973c89001321a1a9a0bc908ca1566ab
Valentin Kim <valentink@yandex.ru>
2024-05-24 17:12:12 +0500

    errors fixed 7

--- celery_worker.py
+++ celery_worker.py
@@ -18,7 +18,7 @@ backend_url = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
 
 # app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
-app = Celery('tasks', broker="redis://redis:6379/0", backend="redis://redis:6379/0")
+app = Celery('tasks', broker=broker_url, backend=backend_url)
 
 app.conf.broker_connection_retry_on_startup = True
 app.conf.broker_connection_max_retries = None
