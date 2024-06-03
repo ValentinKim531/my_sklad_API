ede0b63525db86e2d22977d2912422eb71846a68
Valentin Kim <valentink@yandex.ru>
2024-06-03 18:58:14 +0500

    request interval
    up to 5 min

--- celery_worker.py
+++ celery_worker.py
@@ -33,7 +33,7 @@ app.conf.broker_connection_max_retries = None
 app.conf.beat_schedule = {
     'process-orders-every-3-minutes': {
         'task': 'celery_worker.process_orders',
-        'schedule': 60.0,
+        'schedule': 300.0,
     },
     # 'update-order-statuses-every-10-seconds': {
     #     'task': 'celery_worker.update_order_statuses',
